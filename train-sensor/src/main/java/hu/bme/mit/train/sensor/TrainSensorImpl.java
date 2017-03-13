8bc94cb7ac50c461980fb2e6a84eb594917e683f
GKingA <kinga.andera.gemes@gmail.com>
2017-03-13 08:37:24 -0700

    Alarm

--- train-sensor/src/main/java/hu/bme/mit/train/sensor/TrainSensorImpl.java
+++ train-sensor/src/main/java/hu/bme/mit/train/sensor/TrainSensorImpl.java
@@ -9,10 +9,19 @@ public class TrainSensorImpl implements TrainSensor {
 	private TrainController controller;
 	private TrainUser user;
 	private int speedLimit = 5;
+	private boolean alarm = false;
 
 	public TrainSensorImpl(TrainController controller, TrainUser user) {
 		this.controller = controller;
 		this.user = user;
+	}
+	@Override
+	public  boolean getAlarmState() {
+		return alarm;
+	}
+	@Override
+	public void setAlarmState(boolean alarmState) {
+		alarm = alarmState;
 	}
 
 	@Override
@@ -22,6 +31,15 @@ public class TrainSensorImpl implements TrainSensor {
 
 	@Override
 	public void overrideSpeedLimit(int speedLimit) {
+		if(speedLimit < 0 || speedLimit > 500) {
+			alarm = true;
+		}
+		else if(speedLimit * 2 < controller.getReferenceSpeed()) {
+			alarm = true;
+		}
+		else {
+			alarm = false;
+		}
 		this.speedLimit = speedLimit;
 		controller.setSpeedLimit(speedLimit);
 	}
